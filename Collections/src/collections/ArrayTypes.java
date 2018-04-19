4333370731373c5ff24661b637363ab3d960a25e
elev <elev@172.20.0.17>
2018-04-19 14:29:09 +0200

    ...

---
+++ Collections/src/collections/ArrayTypes.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.Arrays;
+
+public class ArrayTypes {
+
+	public void arrayInt(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++)
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j])
+					
+					
+				System.out.print("  " +arr[i]);
+				
+			}
+
+	}
+
+}
--- Collections/src/collections/Collections.java
+++ Collections/src/collections/Collections.java
@@ -1,10 +1,49 @@
 package collections;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Collections {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] arr = new int[] { 1, 1, 2, 2, 3, 3, 4, 4 };
+
+		ArrayTypes arrayTypes = new ArrayTypes();
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println(Arrays.toString(arr));
+
+		while (true) {
+
+			
+			System.out.println("1. Remove Array dupes");
+			System.out.println("2. Remove Set dupes");
+			System.out.println("3. Add to Array");
+			System.out.println("4. Remove first in Array");
+			System.out.println("5. Map with int + string Array");
+
+			String choice = scan.nextLine();
+
+			if (choice.equals("1")) {
+				scan.next();
+				arrayTypes.arrayInt(arr);
+
+
+			}
+
+			else if (choice.equals("2")) {
+
+			}
+
+			else if (choice.equals("3")) {
+
+			} else if (choice.equals("4")) {
+
+			} else if (choice.equals("5")) {
+
+			}
+		}
 	}
 
 }
